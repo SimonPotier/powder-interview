7c3fc79f34e8a6bc717f5f9a3d3f26aaecb510ee
simm19 <simon@bitit.io>
2023-05-29 23:03:25 +0200

    create useClip hook

---
+++ pages/hooks/useClip.tsx
@@ -0,0 +1,35 @@
+import { useCallback, useMemo, useState } from "react";
+import { Clip } from "../index";
+import Player from "../../components/player";
+
+const useClip = () => {
+  const [currentClip, setCurrentClip] = useState(null);
+  const [clipModalOpen, setClipModalOpen] = useState(false);
+
+  const handleCurrentClip = useCallback(
+    (clip: Clip) => {
+      setCurrentClip(clip);
+      setClipModalOpen(true);
+    },
+    [setCurrentClip, setClipModalOpen]
+  );
+
+  const handleClipModalToggle = (val) => {
+    setClipModalOpen(val);
+  };
+
+  const bodyContent = useMemo(
+    () => <Player clip={currentClip} />,
+    [currentClip]
+  );
+
+  return {
+    handleCurrentClip,
+    handleClipModalToggle,
+    currentClip,
+    clipModalOpen,
+    bodyContent,
+  };
+};
+
+export default useClip;
